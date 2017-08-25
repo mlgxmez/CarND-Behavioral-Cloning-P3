ae90fb90d1c4d6e383593a74b62bf1cfdfb0667c
Guillermo Gómez <mlgxmez@gmail.com>
2017-08-25 21:49:30 +0200

    Cleaning code of model.py

--- model.py
+++ model.py
@@ -2,12 +2,14 @@ import csv
 from scipy.misc import imread
 import numpy as np
 
+# Function to load an image from its path
 def arrange_img(img_path):
 	img_name = img_path.split('/')[-1]
 	path_rel_img = './data/IMG/' + img_name
 	read_img = imread(path_rel_img)
 	return read_img
 
+# Generator to serve data to the model in batches
 def generator(samples_X, samples_y, batch_size=32):
 	num_samples = len(samples_X)
 	while 1:
@@ -21,6 +23,7 @@ def generator(samples_X, samples_y, batch_size=32):
 
 lines = []
 
+# Append path to images to a list
 with open('./data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	next(reader) # Delete this line if you are not using the Udacity data
@@ -31,6 +34,7 @@ images = []
 steering_angles = []
 
 for ll in lines:
+	# The first three columns contain center, left and right image paths
 	image_frame = [arrange_img(ll[i]) for i in range(3)]
 	images = images + image_frame
 	steering = float(ll[3])
@@ -39,6 +43,8 @@ for ll in lines:
 X_data = np.array(images)
 y_data = steering_angles
 
+
+# Split data into train and validation sets
 from sklearn.model_selection import train_test_split
 X_train, X_validation, y_train, y_validation = train_test_split(X_data, y_data, test_size=0.2)
 batch_size = 128
@@ -50,10 +56,12 @@ from keras.models import Model
 from keras.layers import Dense, MaxPooling2D, Input, Cropping2D, Dropout, Lambda, Flatten
 from keras.layers.convolutional import Convolution2D
 
+# Input and preprocessing layers
 img_tensor_0 = Input(shape=(160, 320, 3))
 img_tensor_1 = Lambda(lambda x: (x/255.0) - 0.5)(img_tensor_0)
 img_tensor_2 = Cropping2D(cropping=((50,20), (0,0)))(img_tensor_1)
 
+# Neural network architecture
 x = Convolution2D(6,5,5, activation='relu',border_mode='valid')(img_tensor_2)
 x = MaxPooling2D()(x)
 x = Convolution2D(6,5,5, activation='relu',border_mode='valid')(x)
@@ -67,18 +75,20 @@ x = Dense(512, activation='relu')(x)
 x = Dropout(0.5)(x)
 x = Dense(256, activation='relu')(x)
 x = Dropout(0.5)(x)
-#x = Dense(128, activation='relu')(x)
-#x = Dropout(0.5)(x)
 x = Dense(32, activation='relu')(x)
 
+# Output layer
 predictions = Dense(1)(x)
 
+# Build the model
 model = Model(input=img_tensor_0, output=predictions)
 
-#adam = optimizers.Adam(lr=0.001)
+# Define the loss function and the optimizer
 model.compile(loss='mse', optimizer='rmsprop')
 
+# Train the model using the generator function
 model.fit_generator(train_generator, samples_per_epoch=(len(X_train)//batch_size)*batch_size, validation_data=validation_generator, nb_val_samples=len(X_validation), nb_epoch=5)
 
+# Save the model to drive the car in autonomous mode
 model.save('model.h5')
 print("Keras model has been saved")
